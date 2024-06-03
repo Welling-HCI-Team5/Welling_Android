b098815f34a3cfe8db1551919f64eced4f99b2e3
박 <cksdyd0654@gmail.com>
2024-06-03 19:25:59 +0900

    이동해야하는 화면 추가했음

--- app/src/main/java/com/example/welling/component/NavigationComponent.kt
+++ app/src/main/java/com/example/welling/component/NavigationComponent.kt
@@ -4,24 +4,25 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.welling.MainViewModel
 import com.example.welling.screen.Ability_Donation_Screen
 import com.example.welling.screen.Main_Donation_Screen
 import com.example.welling.screen.ArticleScreen
 import com.example.welling.screen.DonationDetailScreen
 import com.example.welling.screen.Notice_Screen
+import com.example.welling.screen.StartSettingScreen
 
 
 @Composable
 fun NavigationComponent() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "main_donation") {
+    NavHost(navController = navController, startDestination = "start_setting_screen") {
+        composable("start_setting_screen") { StartSettingScreen(navController, mainViewModel = MainViewModel()) }
         composable("main_donation") { Main_Donation_Screen(navController) }
         composable("ability_donation") { Ability_Donation_Screen(navController) }
         composable("article_screen") { ArticleScreen(navController) }
         composable("donation_detail_screen") { DonationDetailScreen(navController) }
         composable("notifications") { Notice_Screen(navController) }
     }
-
-
 }
